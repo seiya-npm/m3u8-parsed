01e42fc71d5f51c2b5c51d3ee0e5fbfdaec5aec7
Seiya <seiya.loveless.mail@gmail.com>
2018-11-10 13:19:23 +0300

    Custom arrays

--- main.js
+++ main.js
@@ -2,12 +2,39 @@
 const m3u8Parser = require('m3u8-parser');
 
 // build variants
-function m3u8(manifest){
+function m3u8(manifest,customParsers){
     let parser = new m3u8Parser.Parser();
+    if(customParsers){
+        for(let p of customParsers){
+            if(p.isArray){
+                p.dataParser = arrayParser(parser, p.customType);
+            }
+            delete p.isArray;
+            parser.addParser(p);
+        }
+    }
     parser.push(manifest);
     parser.end();
-    manifest = parser.manifest;
-    return manifest;
+    return parser.manifest;
+}
+
+// custom array
+function arrayParser(self, name) {
+    return function(line) {
+        let l = line.split(':');
+        l.shift();
+        let data = l.join(':');
+        if(l.length == 1){
+            data = parseFloat(data);
+        }
+        let res = [];
+        let m = this.manifest.custom;
+        if(m !== undefined && m[name] !== undefined){
+            res = m[name];
+        }
+        res.push(data);
+        return res;
+    }.bind(self)
 }
 
 // output
--- test.js
+++ test.js
@@ -1,8 +1,26 @@
 const fs = require('fs');
 const m3u8 = require('./main.js');
 
-const manifest1 = fs.readFileSync("test1.m3u8", "utf8");
+const manifest1 = fs.readFileSync("test/test1.m3u8", "utf8");
 console.log(m3u8(manifest1));
+console.log();
 
-const manifest2 = fs.readFileSync("test2.m3u8", "utf8");
+const manifest2 = fs.readFileSync("test/test2.m3u8", "utf8");
 console.log(m3u8(manifest2));
+console.log();
+
+const manifest3 = fs.readFileSync("test/test3.m3u8", "utf8");
+console.log(m3u8(manifest3,[
+    {
+        expression: /^#VOD-MYVAL(?!3)/,
+        customType: 'myData1',
+        isArray: true
+    },
+    {
+        expression: /^#VOD-MYVAL3/,
+        customType: 'myData3',
+        dataParser: function(line) {
+            return line.split(':')[1];
+        }
+    }
+]));
